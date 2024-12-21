9308a01dbaec7148e3d40806014f173be26e0926
Brian St. Hilaire <brian.st-hilaire@sanctus-tech.com>
2024-12-22 00:24:03 +0100

    Improve text output for warning.

--- src/lampsible/arg_validator.py
+++ src/lampsible/arg_validator.py
@@ -3,6 +3,7 @@ from copy import copy
 from secrets import token_hex
 from warnings import warn
 from getpass import getpass
+from textwrap import dedent
 from requests import head as requests_head
 from fqdn import FQDN
 from ansible_runner import Runner, RunnerConfig
@@ -544,9 +545,17 @@ class ArgValidator():
 
         # Temporary notice about manually adding support for PHP 8.4.
         elif self.args.php_version == '8.4':
-            print('Warning! PHP 8.4 is the latest stable PHP version, but at the moment, no stable Ubuntu distribution includes that in its default repositories. What you are trying to do will not work without manually configuring your server\'s APT repositories. Run these commands first on the server:')
+            print(dedent("""
+                Warning! PHP 8.4 is the latest stable PHP version, but at the
+                moment, Ubuntu package repositories are still configured
+                for PHP 8.3.
+                What you are trying to do will not work without manually
+                configuring your server's APT repositories.
+
+                Run these commands on the server before proceeding:
+            """))
             print('sudo apt install software-properties-common')
-            print('sudo add-apt-repository ppa:ondrej/php')
+            print('sudo add-apt-repository ppa:ondrej/php\n')
 
         # User passed a value, warn them if it's likely to not work.
         # TODO: In the future, we should have a global "non-interactive" flag,
